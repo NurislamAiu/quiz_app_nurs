b63e7778bcbf3f47c503c8e96eedb976c633c62f
nurislamilyasov <nurikileasov0000@gmail.com>
2025-03-09 20:39:27 +0500

    created functions for random answer to questions(shuffle)

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -15,34 +15,28 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
     final currentQuestion = questions[0];
-    return SizedBox(
-      width: double.infinity,
-      height: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 30),
-          AnswerButton(
-            onTap: () {},
-            answerText: currentQuestion.answers[0],
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: currentQuestion.answers[1],
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: currentQuestion.answers[2],
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: currentQuestion.answers[3],
-          ),
-        ],
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
