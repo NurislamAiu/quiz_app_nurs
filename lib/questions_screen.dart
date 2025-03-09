24b60859308b3a105fa2364ee54699c96d57fa13
nurislamilyasov <nurikileasov0000@gmail.com>
2025-03-09 21:45:24 +0500

    creating result screen, added functions add selected answers to Map

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,9 @@ import 'package:quiz_app_nurs/answer_button.dart';
 import 'package:quiz_app_nurs/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +17,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -39,13 +42,16 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
                 color: Colors.white,
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-              ),textAlign: TextAlign.center,),
+              ),
+              textAlign: TextAlign.center,
+            ),
             SizedBox(height: 30),
-            ...currentQuestion.getShuffledAnswers().map(
-              (answer) {
+            ...currentQuestion.getShuffledAnswers().map((answer) {
                 return AnswerButton(
                   answerText: answer,
-                  onTap: answerQuestion,
+                  onTap: (){
+                    answerQuestion(answer);
+                  },
                 );
               },
             ),
